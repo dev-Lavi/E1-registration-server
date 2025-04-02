e2919059201b699f034d9f665110b12bb376dbf5
Prakriti <anukriti.1911@gmail.com>
2025-04-03 01:54:37 +0530

    html file added

--- api/teamRoutes.js
+++ api/teamRoutes.js
@@ -34,21 +34,30 @@ const teamSchema = Joi.object({
 
 // Route: Register a new team
 router.post("/register", async (req, res) => {
-    const {"g-recaptcha-response": recaptchaToken, ...teamData} = req.body; 
+    
+    const recaptchaToken = req.body["g-recaptcha-response"];
+
+    console.log("Token:\n",recaptchaToken)
     if(!recaptchaToken){
         return res.status(400).json({message:"reCAPTCHA challenge incomplete"})
     }
     try{
-        const response = await axios.post(``, null,{
+        const response = await axios.post("https://www.google.com/recaptcha/api/siteverify", null,{
             params:{
                 secret: process.env.reCAPTCHA_secret_key,
                 response: recaptchaToken
             }
         })
         if(!response.data.success){
+            console.log("reCAPTCHA failed")
             return res.status(400).json({message:"reCAPTCHA verification failed!"})
         }
+        else
+        console.log("reCAPTCHA verification successful")
         
+        delete req.body["g-recaptcha-response"]; 
+        const teamData = req.body;
+
         const { error} = teamSchema.validate(req.body);
         if (error) {
             return res.status(400).json({ message: error.details[0].message });
@@ -57,6 +66,7 @@ router.post("/register", async (req, res) => {
         const newTeam = new Team(teamData);
         await newTeam.save();
         res.status(201).json({ message: "Team registered successfully!" });
+        console.log("Team registered")
     } catch (err) {
         res.status(500).json({ message: "Server error, please try again later." });
     }
