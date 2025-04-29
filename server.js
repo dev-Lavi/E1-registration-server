3d171b171ea0814d81f5437e280853f4d52dc8c4
Lavi sharma <lavipandit123@gmail.com>
2025-04-29 17:05:59 +0530

    added specefic domain

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ import helmet from "helmet";
 import cors from "cors";
 
 const allowedOrigins = [
-  'https://www.kryptonmind.tech',
+  'https://www.kryptonmind.tech/register',
   'http://localhost:5173' 
 ];
 
