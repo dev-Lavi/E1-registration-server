b2bc6e9abf606156d63390ac920bf4d026098495
Lavi sharma <lavipandit123@gmail.com>
2025-05-05 03:58:08 +0530

    added captch on send otp route

--- middleware/rateLimiter.js
+++ middleware/rateLimiter.js
@@ -7,8 +7,8 @@ const getRealIP = (req) => {
 
 // Create the rate limiter middleware
 const rateLimiter = rateLimit({
-  windowMs: 10 * 60 * 1000, // 10 minutes
-  max: 10, // limit each IP to 10 requests per windowMs
+  windowMs: 1 * 60 * 1000, // 10 minutes
+  max: 3, // limit each IP to 10 requests per windowMs
   keyGenerator: (req) => getRealIP(req), // Use the real IP from Cloudflare's header
   message: "Too many requests, please try again later.", // Custom message
 });
