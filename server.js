c4ae82a1f4ece28ab2e7de9822dd4def954835aa
Lavi sharma <lavipandit123@gmail.com>
2025-05-04 13:58:29 +0530

    added domain frontend

--- server.js
+++ server.js
@@ -18,8 +18,8 @@ if (process.env.NODE_ENV === "production") {
   app.set("trust proxy", 1);
 }
 
-app.use(cors({ 
-  origin: ['https://registration.teamconatus.com', 'https://www.kryptonmind.tech'],
+app.use(cors({
+  origin: 'https://www.kryptonmind.tech',
   credentials: true 
 }));
 
