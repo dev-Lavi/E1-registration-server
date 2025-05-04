bff84878f7a511a477ccf092bb235945964c23a2
Lavi sharma <lavipandit123@gmail.com>
2025-05-05 04:33:09 +0530

    added seperate rate limiters for both routes

--- server.js
+++ server.js
@@ -17,10 +17,7 @@ const app = express();
 
 app.set("trust proxy", 1);
 
-app.use(cors({  
-  origin: 'https://registration.teamconatus.com',
-  credentials: true 
-}));
+app.use(cors());
 
 
 app.use(express.json());
