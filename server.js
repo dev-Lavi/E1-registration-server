69d4c79c0d889a7bfdac6690e5f25ff274f2d65d
Lavi sharma <lavipandit123@gmail.com>
2025-05-05 16:21:13 +0530

    added seperate rate limiters for both routes

--- server.js
+++ server.js
@@ -26,7 +26,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(mongoSanitize());
 app.use(helmet());
-app.use(rateLimiter);
+
 
 
 app.use("/api/teams", teamRoutes);
