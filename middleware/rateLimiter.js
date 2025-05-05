232b5c271a9a7add113fa06ae2a51791f54ff7bf
Lavi sharma <lavipandit123@gmail.com>
2025-05-05 15:54:37 +0530

    added seperate rate limiters for both routes

--- middleware/rateLimiter.js
+++ middleware/rateLimiter.js
@@ -1,6 +1,6 @@
 import rateLimit from "express-rate-limit"; 
 const rateLimiter = rateLimit({
-  windowMs: 10 * 60 * 1000,
+  windowMs: 100 * 60 * 1000,
   max: 27,
   message: "Too many requests, please try again later.",
 });
